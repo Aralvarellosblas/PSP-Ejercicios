57fb5a19c08e8f136a5a8c3f0ef663df9b818427
arturo <arturo@ArturoMSI>
2018-11-29 09:19:44 +0100

    Añadidos comentarios

--- Ejercicio2/src/ejercicio2/Ejercicio2.java
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -1,15 +1,20 @@
 package ejercicio2;
 
 /**
+ * Esta clase principal contiene las llamadas a los metodos para que funcione el
+ * programa
  *
  * @author Arturo
  */
-public class Ejercicio2 {
+public class Ejercicio2{
 
     /**
+     * El metodo main inicia 4 hilos diferentes y muestra su mensaje de
+     * finalización
+     *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new Hilo("a").start();
         new Hilo("b").start();
         new Hilo("c").start();
--- Ejercicio2/src/ejercicio2/Hilo.java
+++ Ejercicio2/src/ejercicio2/Hilo.java
@@ -4,18 +4,31 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * En esta clase Hilo se define un constructor para la clase, y lo que debe
+ * hacer dicho hilo cuando se inicia
  *
  * @author Arturo
  */
 public class Hilo extends Thread implements Runnable{
 
+    /**
+     * Constructor de la clase hilo
+     *
+     * @param st nombre del hilo
+     */
     public Hilo(String st){
         super(st);
     }
 
+    /**
+     * El hilo se ejecuta 5 veces, mostrando en cada una un mensaje con el
+     * numero de iteración y su nombre, después espera durante un segundo. Una
+     * vez terminadas todas las iteraciones el hilo muestra un mensaje de
+     * despedida
+     */
     @Override
     public void run(){
-        for(int i=0; i<10; i++){
+        for(int i=0; i<5; i++){
             try{
                 System.out.println(i+" "+getName());
                 Hilo.sleep(1000);
