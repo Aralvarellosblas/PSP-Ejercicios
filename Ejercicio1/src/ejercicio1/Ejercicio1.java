a069c37c8e337e7776ca2d43e0bc3e3a73304f7f
arturo <arturo@ArturoMSI>
2018-11-29 09:13:05 +0100

    Añadidos comentarios

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,15 +1,20 @@
 package ejercicio1;
 
 /**
+ * Esta clase principal contiene las llamadas a los metodos para que funcione el
+ * programa
  *
  * @author Arturo
  */
-public class Ejercicio1 {
+public class Ejercicio1{
 
     /**
+     * Desde la clase main se inician los dos hilos y se muestra el mensaje de
+     * finalización del main
+     *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new NuevoHilo("a").start();
         new NuevoHilo("b").start();
         System.out.println("Termina clase main");
--- Ejercicio1/src/ejercicio1/NuevoHilo.java
+++ Ejercicio1/src/ejercicio1/NuevoHilo.java
@@ -4,22 +4,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * En esta clase Hilo se define un constructor para la clase, y lo que debe
+ * hacer dicho hilo cuando se inicia
  *
  * @author Arturo
  */
 public class NuevoHilo extends Thread implements Runnable{
 
+    /**
+     * Constructor del Hilo
+     *
+     * @param st Nombre del hilo
+     */
     public NuevoHilo(String st){
         super(st);
     }
 
+    /**
+     * En el metodo run se define lo que hace el hilo cuando se ejecuta
+     *
+     * En este caso el hilo muestra sun numero de iteración y su nombre y a
+     * continuación espera un tiempo aleatorio antes de mostrar la siguiente
+     * iteración. Tambien incluye un mensaje de fin de ejecución
+     */
     @Override
     public void run(){
         for(int i=0; i<10; i++){
             try{
                 System.out.println(i+" "+getName());
-                long s=(long) ((Math.random()*2000)+1);
-                NuevoHilo.sleep(s);
+                long s=(long) ((Math.random()*2000)+1);//selecciona aleatoriamente un numero ente 1 y 2000
+                NuevoHilo.sleep(s); //para el hilo durante un numero de millisegundos s
             }catch(InterruptedException ex){
                 Logger.getLogger(NuevoHilo.class.getName()).log(Level.SEVERE, null, ex);
             }
