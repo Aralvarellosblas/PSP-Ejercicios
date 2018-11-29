91f01b9f2ba902b269f05dab8b9d53a07e314396
arturo <arturo@ArturoMSI>
2018-11-29 09:40:02 +0100

    Añadidos comentarios

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,16 +1,23 @@
 package ejercicio3;
 
 /**
+ * Esta clase principal contiene las llamadas a los metodos para que funcione el
+ * programa
  *
  * @author Arturo
  */
-public class Ejercicio3 {
+public class Ejercicio3{
 
     /**
+     * En este main tenemos una variable de tipo int denominada contador que nos
+     * permite llevar la cuenta de cuantos hilos se han creado. Iniciamos el
+     * hilo, pasandole el parametro del contador para que pueda saber que hilo
+     * es el que crea
+     *
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int contador = 0;
+    public static void main(String[] args){
+        int contador=0;
         new Hilo(contador).start();
     }
 
--- Ejercicio3/src/ejercicio3/Hilo.java
+++ Ejercicio3/src/ejercicio3/Hilo.java
@@ -4,20 +4,40 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * En esta clase Hilo se define un constructor para la clase, y lo que debe
+ * hacer dicho hilo cuando se inicia
  *
  * @author Arturo
  */
 public class Hilo extends Thread implements Runnable{
-    Hilo h;
+
+    Hilo h;//Variable de tipo hilo que permite al constructor iniciar a los hilos hijos
+
+    /**
+     * El constructor de la clase se encarga de aumentar el contador del hilo,
+     * mostrar el mensaje de saludo y controlar que no se creen más hilos de los
+     * necesarios
+     *
+     * @param contador numero de hilos que se han creado
+     */
     public Hilo(int contador){
-        contador=contador+1;
+        contador=contador+1; //Se incrementa el contador cada vez  que creamos un nuevo hilo
         System.out.println(getName()+" empezó");
+        //El condicional comprueba que no se creen mas de 5 hilos
         if(contador<5){
-           h=new Hilo(contador);
-           h.start();
+            h=new Hilo(contador);
+            h.start();
         }
     }
 
+    /**
+     * El hilo se ejecuta 10 vececs mostrando su nombre y el numero de
+     * iteración, a continuación espera un numero aleatorio de milisegundos
+     * (entre 1 y 600) Si el nombre del hilo que se ejecuta no es el 4, además
+     * el hilo realiza un join, lo que significa que espera a que el siguiente
+     * hilo termine antes de mostrar su ultimo mensaje. Tambien muestra un
+     * mensaje de finalización
+     */
     @Override
     public void run(){
 
@@ -29,6 +49,7 @@ public class Hilo extends Thread implements Runnable{
                 Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        //Evita que el ultimo hilo se quede esperando dejando colgado todo el programa
         if(!getName().equals("Thread-4")){
             try{
                 h.join();
